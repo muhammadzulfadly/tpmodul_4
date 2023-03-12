0559fc59f70a14404d4817cddb82ee4ae6369382
muhammadzulfadly <121471995+muhammadzulfadly@users.noreply.github.com>
2023-03-13 03:07:39 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace tpmodul_4
+{
+	public class DoorMachine
+	{
+        private enum State
+        {
+            TERKUNCI,
+            TERBUKA
+        }
+
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.TERKUNCI;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void bukaPintu()
+        {
+            currentState = State.TERBUKA;
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+}
+
